36ff850e1ef6a916af3265e78f51b228244ec4ee
mqcarpenter <81033949+mqcarpenter@users.noreply.github.com>
2026-08-30 10:06:22 -0400

    Add front scan for 1998 Donruss Elite Aspirations #66

--- migrate-012-update-new-scans.sql
+++ migrate-012-update-new-scans.sql
@@ -33,3 +33,4 @@ UPDATE cards SET image = '1998_Pacific-Invincible-Gems-of-the-Diamond_130-Fr.jpg
 UPDATE cards SET image = '1999_Pacific-Omega-Gold_47-Fr.jpg' WHERE id = '1999-47-1999-pacific-omega-gold';
 UPDATE cards SET image = '1996_Topps-Chrome-Refractors_20-Fr.jpg' WHERE id = '1996-20-1996-topps-chrome-refractors';
 UPDATE cards SET image = '1997_Bowman-Chrome-Refractors_64-Fr.jpg' WHERE id = '1997-64-1997-bowman-chrome-refractors';
+UPDATE cards SET image = '1998_Donruss-Elite-Aspirations_66-Fr.jpg' WHERE id = '1998-66-1998-donruss-elite-aspirations';
